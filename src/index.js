39ded1f505f764ace0dd7332b54be71a54277eda
Ryan Gallagher <97428947+rjjgallagher@users.noreply.github.com>
2023-07-15 17:53:36 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs')
+const fs = require('fs')
 const path = require('node:path')
 // Require the necessary discord.js classes
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
