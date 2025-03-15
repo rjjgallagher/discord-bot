1d58a10b40485f43985f19e9c251fad91190eb40
rjjgallagher <rjjgallagher@gmail.com>
2025-03-15 03:25:42 -0500

    created connect to database function

---
+++ src/database/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+async function connectToDatabase() {
+    const dbUrl = process.env.MONGO_DB_URL;
+
+    if (!dbUrl) {
+        throw new Error("MONGO_DB_URL is not defined in environment variables.");
+    }
+
+    try {
+        await mongoose.connect(db_Url);
+        console.log("Database connected");
+    } catch (err) {
+        console.error("Connection error:", err);
+    }
+}
